660a2d9af805b05757bfea09724ec0b038ef42a2
Valerio Spagnoli <spagnolivalerio23@gmail.com>
2026-03-05 16:43:09 +0100

    quick commit

--- vite-cont/vite.config.js
+++ vite-cont/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  root: "src",
   server: {
+    host: "0.0.0.0",
+    port: 5001,
+    watch: {
+      usePolling: true,
+      interval: 200,
+    },
     proxy: {
       "/api": {
         target: "http://data-processing-cont:5000",
